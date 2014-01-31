af9e2bc47a2f254790dd45eb347fe3e3f9ef9194
Yuka Kato <y-kato@esm.co.jp>
2014-01-31 10:22:05 +0900

    Modify method description

--- create_report.rb
+++ create_report.rb
@@ -5,7 +5,7 @@ class Copy_file < Thor
   include Thor::Actions
 
   desc 'nippo', 'Create new nippo file including specify date'
-  # Thor::Actions::source_root このクラスのルートディレクトリを設定する
+  # Thor::Actions::source_root … Thor::Actions#copy_file や Thor::Actions#template メソッドにおけるコピー元のディレクトリを設定する
   def self.source_root
     # __FILE__ 現在のソースファイル名（フルパスとは限らない）
     # File.dirname(filename) 引数 filename のうち、スラッシュより前を返却する（ディレクトリ名にあたるため）。
